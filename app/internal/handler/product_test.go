b5465c495619b7f2f11a6a84baead4ac44eb15db
Alejandro N. Rodriguez <alejandronazareno.rodriguez@mercadolibre.com>
2024-01-12 14:11:16 -0300

    test: add test for GetProductByID

--- app/internal/handler/product_test.go
+++ app/internal/handler/product_test.go
@@ -1,6 +1,7 @@
 package handler_test
 
 import (
+	"context"
 	"goweb/app/internal"
 	"goweb/app/internal/handler"
 	"goweb/app/internal/repository"
@@ -10,6 +11,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/stretchr/testify/require"
 )
 
@@ -61,4 +63,54 @@ func TestGetAllProducts(t *testing.T) {
 		require.Equal(t, expectedHeader, res.Header())
 	})
 
+}
+
+func TestGetProductByID(t *testing.T) {
+	t.Run("Obtener el producto con el id solicitado.", func(t *testing.T) {
+		// Arrange
+		data := map[int]internal.Product{
+			1: {
+				ID:          1,
+				Name:        "Producto 1",
+				Quantity:    10,
+				CodeValue:   "123456",
+				IsPublished: true,
+				Expiration:  time.Date(2021, 12, 31, 0, 0, 0, 0, time.UTC),
+				Price:       100,
+			},
+			2: {
+				ID:          2,
+				Name:        "Producto 2",
+				Quantity:    20,
+				CodeValue:   "654321",
+				IsPublished: false,
+				Expiration:  time.Date(2021, 12, 31, 0, 0, 0, 0, time.UTC),
+				Price:       200,
+			},
+		}
+		repo := repository.NewRepositoryMap(data)
+		service := service.NewProductService(repo)
+		handler := handler.NewProductHandler(service)
+
+		res := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/products/1", nil)
+		// add id path param to the request
+		chiCtx := chi.NewRouteContext()
+		chiCtx.URLParams.Add("id", "1")
+		context := context.WithValue(req.Context(), chi.RouteCtxKey, chiCtx)
+		req = req.WithContext(context)
+
+		// Act
+		handler.GetProductByID(res, req)
+
+		// Assert
+		expectedCode := http.StatusOK
+		expectedBody := `{"id":1,"name":"Producto 1","quantity":10,"code_value":"123456","is_published":true,"expiration":"31/12/2021","price":100}`
+		expectedHeader := http.Header{
+			"Content-Type": []string{"application/json"},
+		}
+		require.Equal(t, expectedCode, res.Code)
+		require.JSONEq(t, expectedBody, res.Body.String())
+		require.Equal(t, expectedHeader, res.Header())
+	})
 }
